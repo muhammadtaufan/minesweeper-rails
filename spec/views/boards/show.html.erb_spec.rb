c9a39b6f144e10a262d2b7e5ed3ffbc36bf8d69c
mtaufan.dev <mtaufan.dev@gmail.com>
2023-07-27 11:00:14 +0700

    add links on ui and add spec on show boards

--- spec/views/boards/show.html.erb_spec.rb
+++ spec/views/boards/show.html.erb_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe "boards/show.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'boards/show.html.erb', type: :view do
+  before(:each) do
+    @board = assign(:board, Board.create!(
+                              name: 'Test Board',
+                              email: 'test@example.com',
+                              width: 5,
+                              height: 5,
+                              mines: 3,
+                              cells: "00000\n00000\n00000\n00000\n00000"
+                            ))
+  end
+
+  it 'renders board details' do
+    render
+    expect(rendered).to match(/Test Board/)
+    expect(rendered).to match(/test@example.com/)
+    expect(rendered).to match(/5/)
+    expect(rendered).to match(/3/)
+  end
+
+  it 'renders the board cells' do
+    render
+    assert_select 'div.cells', count: 5
+    assert_select 'div.mines', count: 3
+    assert_select 'div.boards', count: 25
+  end
+
+  it 'renders back link' do
+    render
+    assert_select 'a[href=?]', boards_path, text: 'Back'
+  end
 end
